cf11beb1384fc0a4622da4d9bd1fcca5e707cf37
Justincadburywong <justincadburywong@gmail.com>
2018-03-30 23:10:54 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+
+# PostgreSQL driver
+gem 'pg'
+
+# Sinatra driver
+gem 'sinatra'
+gem 'sinatra-contrib'
+
+# Use Thin for our web server
+gem 'thin'
+gem 'dotenv'
+gem 'activesupport'
+gem 'activerecord'
+gem 'rake'
+gem 'bcrypt'
+gem 'pry-byebug'
+gem 'shotgun'
+gem 'faker'
+gem 'rspec'
+
+group :test do
+  gem 'rspec'
+  gem 'rack-test'
+  gem 'webmock'
+  gem 'database_cleaner'
+  gem 'coveralls', require: false
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,30 @@
+require 'coveralls'
+Coveralls.wear!
+require 'rubygems'
+require 'webmock/rspec'
+require 'database_cleaner'
+require 'rack/test'
+# All our specs should require 'spec_helper' (this file)
+
+# If RACK_ENV isn't set, set it to 'test'.  Sinatra defaults to development,
+# so we have to override that unless we want to set RACK_ENV=test from the
+# command line when we run rake spec.  That's tedious, so do it here.
+ENV['RACK_ENV'] ||= 'test'
+
+require File.expand_path("../../config/environment", __FILE__)
+
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) { DatabaseCleaner.start }
+  config.after(:each)  { DatabaseCleaner.clean }
+end
+
+def app
+  Sinatra::Application.new
+end
