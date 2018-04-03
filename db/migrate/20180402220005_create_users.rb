fbd0d76c2da839438f672604125d4960220f7ace
Justincadburywong <justincadburywong@gmail.com>
2018-04-02 22:17:01 -0700

    migrate db and edit rakefile

--- db/migrate/20180402220005_create_users.rb
+++ db/migrate/20180402220005_create_users.rb
@@ -1,8 +1,10 @@
-class CreateUsers < ActiveRecord::Migration
+class CreateUsers < ActiveRecord::Migration[5.1]
   def change
-    t.string :email, null: false
-    t.string :password_digest, null: false
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :password_digest, null: false
 
-    t.timestamps null: false
+      t.timestamps null: false
+    end
   end
 end
