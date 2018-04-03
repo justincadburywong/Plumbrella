99db9b807e590cb54c928ba8488d9a711a32c601
Justincadburywong <justincadburywong@gmail.com>
2018-04-02 21:57:13 -0700

    add logic and fields

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -5,17 +5,22 @@ end
 
 post '/login' do
   #find user based on email address
-  @user = User.find_by(username: params[:username])
+  @user = User.find_by(email: params[:email])
   #validate user based on valid password
   #if it does
   #&& is checking if user is not nill !!
-  if @user && @user.(params[:password])
-    #set the user-id to session
-    session[:id] = @user.id
-    redirect '/'
+  if @user
+    if @user.password == params[:password]
+      #set the user-id to session
+      session[:id] = @user.id
+      redirect '/'
+    else
+      @error = "Your password or email was incorrect"
+      erb :'/sessions/new'
+    end
   else
-    @error = "Your password or email was incorrect"
-    erb :'/sessions/new'
+    @error = "Email not found.  You need to sign up first"
+    erb :'/users/new'
   end
 end
 
